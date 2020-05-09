2d58226b0613c1e2e0b997ddf0036fd19858946d
txsgit <909083393@qq.com>
2020-05-09 19:30:01 +0800

    ''

--- postcss.config.js
+++ postcss.config.js
@@ -11,7 +11,7 @@ module.exports = {
         selectorBlackList: [],  // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
         minPixelValue: 1,       // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
         mediaQuery: false,       // 允许在媒体查询中转换`px`
-        exclude: /(\/|\\)(node_modules)(\/|\\)/     //忽略文件文件或文件夹下的样式
+        exclude: [/(\/|\\)(tabbar)(\/|\\)/,/(\/|\\)(navbar)(\/|\\)/]     //忽略文件文件或文件夹下的样式
       }
     }
   }
